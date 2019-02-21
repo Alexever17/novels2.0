b3e3521f3e182570cd40362790124dfa191cc9d1
alexever17@gmail.com <alexever17@gmail.com>
2019-02-21 22:42:21 +0100

    Updates

---
+++ precache-manifest.63515b1814cd4a842f341df965d66906.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d26e642a660e8b0a75ea5d8d18853b7f",
+    "url": "/novels2.0/static/media/logo.d26e642a.png"
+  },
+  {
+    "revision": "ff30448fefab04caf664",
+    "url": "/novels2.0/static/css/main.59e5c70c.chunk.css"
+  },
+  {
+    "revision": "22f25bb4a14393dfc68e",
+    "url": "/novels2.0/static/js/1.22f25bb4.chunk.js"
+  },
+  {
+    "revision": "c77cb7f3a9400c53fade",
+    "url": "/novels2.0/static/js/runtime~main.c77cb7f3.js"
+  },
+  {
+    "revision": "93b93aeac8b89f8f2d3dfe92f89ced42",
+    "url": "/novels2.0/static/media/pipes.93b93aea.png"
+  },
+  {
+    "revision": "ff30448fefab04caf664",
+    "url": "/novels2.0/static/js/main.ff30448f.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/novels2.0/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/novels2.0/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/novels2.0/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/novels2.0/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5ccf5ac24143054fc249eeb957a23cfc",
+    "url": "/novels2.0/index.html"
+  }
+];
