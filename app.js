ad9924a9b21f7be878a9001cebb377a06ef1b033
Alexever17 <alexever17@gmail.com>
2019-05-30 01:27:31 +0200

    lesson  13

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ new Vue({
     title: 'Becoming a Vue Ninja',
     name: 'Oleksandr Malakhov',
     url: 'https://novelslist.com',
-    classes: ["one", "two"]
+    classes: ["one", "two"],
+    wage: 10,
+    coords: { x: 0, y: 0},
+    jeff: "Jeff"
   },
   methods: {
     greet(time) {
       return `Hello and good ${time}, ${this.name}. Lets rocks!`
+    },
+    changeWage(amount) {
+      this.wage += amount;
+    },
+    logEvent(e) {
+      console.log(e)
+    },
+    logCoords(e) {
+      this.coords.x = e.offsetX;
+      this.coords.y = e.offsetY;
+    },
+    updateName(e) {
+      //console.log(e.target.value)
+      this.jeff = e.target.value;
+    },
+    logMessage() {
+      console.log("hello console window")
     }
   }
 })
